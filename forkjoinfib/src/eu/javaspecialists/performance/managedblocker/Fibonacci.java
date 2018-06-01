24e78ad8b66c2596461feb6ccbb70b3a25fa69a9
kabutz <heinz@javaspecialists.eu>
2018-06-01 18:29:32 +0300

    Final solution

--- forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
+++ forkjoinfib/src/eu/javaspecialists/performance/managedblocker/Fibonacci.java
@@ -1,6 +1,7 @@
 package eu.javaspecialists.performance.managedblocker;
 
 import java.math.*;
+import java.util.*;
 import java.util.concurrent.*;
 
 // test100_000_000() time = 37729
@@ -9,26 +10,71 @@ import java.util.concurrent.*;
 
 // TODO: Sign up to Heinz's Newsletter: www.javaspecialists.eu
 public class Fibonacci {
+    private final BigInteger RESERVED = BigInteger.valueOf(-1000);
+
     public BigInteger f(int n) {
         if (n < 0) throw new IllegalArgumentException("n < 0");
-        if (n == 0) return BigInteger.ZERO;
-        if (n == 1) return BigInteger.ONE;
+        Map<Integer, BigInteger> cache = new ConcurrentHashMap<>();
+        cache.put(0, BigInteger.ZERO);
+        cache.put(1, BigInteger.ONE);
+        return f(n, cache);
+    }
 
-        int half = (n + 1) / 2;
+    private BigInteger f(int n, Map<Integer, BigInteger> cache) {
+        BigInteger result = cache.putIfAbsent(n, RESERVED);
+        if (result == null) {
+            int half = (n + 1) / 2;
 
-        RecursiveTask<BigInteger> f0task = new RecursiveTask<BigInteger>() {
-            protected BigInteger compute() {
-                return f(half-1);
+            RecursiveTask<BigInteger> f0task = new RecursiveTask<BigInteger>() {
+                protected BigInteger compute() {
+                    return f(half - 1, cache);
+                }
+            };
+            f0task.fork();
+            BigInteger f1 = f(half, cache);
+            BigInteger f0 = f0task.join();
+
+            if (n % 2 == 1) {
+                result = f0.multiply(f0).add(f1.multiply(f1));
+            } else {
+                result = f0.shiftLeft(1).add(f1).multiply(f1);
             }
-        };
-        f0task.fork();
-        BigInteger f1 = f(half);
-        BigInteger f0 = f0task.join();
+            synchronized (RESERVED) {
+                cache.put(n, result);
+                RESERVED.notifyAll();
+            }
+        } else if (result == RESERVED) {
+            try {
+                ReservedBlocker blocker = new ReservedBlocker(n, cache);
+                ForkJoinPool.managedBlock(blocker);
+                result = blocker.result;
+            } catch (InterruptedException e) {
+                throw new CancellationException();
+            }
+        }
+        return result;
+    }
+    private class ReservedBlocker implements ForkJoinPool.ManagedBlocker {
+        private BigInteger result;
+        private final int n;
+        private final Map<Integer, BigInteger> cache;
 
-        if (n % 2 == 1) {
-            return f0.multiply(f0).add(f1.multiply(f1));
-        } else {
-            return f0.shiftLeft(1).add(f1).multiply(f1);
+        public ReservedBlocker(int n, Map<Integer, BigInteger> cache) {
+            this.n = n;
+            this.cache = cache;
+        }
+
+        public boolean isReleasable() {
+            return (result = cache.get(n)) != RESERVED;
+        }
+
+        public boolean block() throws InterruptedException {
+            synchronized (RESERVED) {
+                while(!isReleasable()) {
+                    RESERVED.wait();
+                }
+            }
+            return true;
         }
     }
 }
--- run.sh
+++ run.sh
@@ -7,4 +7,4 @@ mkdir -p out/production/mathhack
 cd mathhack/src
 javac -Xlint:none -d ../../out/production/mathhack  java/math/BigInteger.java
 cd ../../
-java -showversion -Xbootclasspath/p:/Users/heinz/Dropbox/heinz/projects/talks/18_VoxxedAthens/TurboChargeManagedBlockerVoxxedAthens2018/out/production/mathhack -Djava.util.concurrent.ForkJoinPool.common.parallelism=3 -cp out/production/forkjoinfib -verbose:gc -Xmx8g -Xms8g -XX:NewSize=4g -XX:SurvivorRatio=1 eu.javaspecialists.performance.managedblocker.FibonacciPerfTest
+time java -showversion -Xbootclasspath/p:/Users/heinz/Dropbox/heinz/projects/talks/18_VoxxedAthens/TurboChargeManagedBlockerVoxxedAthens2018/out/production/mathhack -Djava.util.concurrent.ForkJoinPool.common.parallelism=3 -cp out/production/forkjoinfib -verbose:gc -Xmx8g -Xms8g -XX:NewSize=4g -XX:SurvivorRatio=1 eu.javaspecialists.performance.managedblocker.FibonacciPerfTest
